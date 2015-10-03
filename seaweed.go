0b93061375f56515315396708730d04e34410e1f
Mike Ball <mikedball@gmail.com>
2015-10-03 12:16:46 -0400

    initial commit

---
+++ seaweed.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/codegangsta/cli"
+	"github.com/crackcomm/go-clitable"
+	"github.com/mdb/seaweed"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "seaweed-cli"
+	app.Usage = "Should I go surfing?"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "apiKey",
+			Usage:  "Magic Seaweed API key",
+			EnvVar: "MAGIC_SEAWEED_API_KEY",
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:        "forecast",
+			Usage:       "forcast <spotId>",
+			Description: "View the forecast for a spot",
+			Action:      forecast,
+		},
+	}
+	app.Run(os.Args)
+}
+
+func forecast(c *cli.Context) {
+	client := client(c)
+	spot := c.Args().First()
+	forecast, err := client.Forecast(spot)
+	if err != nil {
+		panic(err)
+	}
+
+	s := []map[string]interface{}{}
+	for _, each := range forecast {
+		m := map[string]interface{}{}
+		m["Date"] = time.Unix(each.LocalTimestamp, 0).Format("Mon 01/02 03:04 pm")
+		m["Solid Rating"] = each.SolidRating
+		m["Faded Rating"] = each.FadedRating
+		m["Primary Swell Height"] = concat([]string{strconv.FormatFloat(each.Swell.Components.Primary.Height, 'f', 2, 64), each.Swell.Unit})
+		m["Wind"] = concat([]string{strconv.Itoa(each.Wind.Speed), " ", each.Wind.Unit, " ", each.Wind.CompassDirection})
+		s = append(s, m)
+	}
+
+	if len(s) != 0 {
+		clitable.PrintTable([]string{"Date", "Primary Swell Height", "Wind", "Solid Rating", "Faded Rating"}, s)
+	} else {
+		fmt.Printf("No forecast found for spot: %s\n", spot)
+	}
+
+}
+
+func client(c *cli.Context) *seaweed.Client {
+	return seaweed.NewClient(os.Getenv("MAGIC_SEAWEED_API_KEY"))
+}
+
+func concat(arr []string) string {
+	var buff bytes.Buffer
+
+	for _, elem := range arr {
+		buff.WriteString(elem)
+	}
+
+	return buff.String()
+}
