61c1d1d74af86a061f3e6d5105b760c748716c3c
Mike Ball <mikedball@gmail.com>
2016-03-07 21:40:20 -0500

    only pass option to sed if Mac OSX

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -14,5 +14,11 @@ teardown() {
 fixture() {
   cp test/{fixtures,tmp}/seaweed_391
   today=`date +%s`
-  sed -i "bak" "s/today_timestamp/$today/g" test/tmp/seaweed_391
+  opt=""
+
+  if [ "$(uname)" == "Darwin" ]; then
+    opt='-i "bak"'
+  fi
+
+  sed $opt "s/today_timestamp/$today/g" test/tmp/seaweed_391
 }
